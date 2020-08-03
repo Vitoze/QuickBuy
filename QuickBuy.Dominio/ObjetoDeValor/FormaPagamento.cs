594972f0ba8bc7fe0d9f80c80288903f6af7151b
Vitor Barros <vitibarros.brandao@gmail.com>
2020-08-03 14:34:29 +0100

    Create Entities

---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,36 @@
+﻿using QuickBuy.Dominio.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool Boleto
+        {
+            get { return Id == (int)TipoFormaPagamento.Boleto; }
+        }
+
+        public bool Deposito
+        {
+            get { return Id == (int)TipoFormaPagamento.Deposito; }
+        }
+
+        public bool CartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamento.CartaoCredito; }
+        }
+
+        public bool NaoDefinido
+        {
+            get { return Id == (int)TipoFormaPagamento.NaoDefinido; }
+        }
+
+
+    }
+}
